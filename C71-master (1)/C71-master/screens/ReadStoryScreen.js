ebc0dd95b06cf2196432780847160c962d471753
TheRealNeathed <64224708+TheRealNeathed@users.noreply.github.com>
2020-11-16 10:33:11 -0500

    Update ReadStoryScreen.js

--- C71-master (1)/C71-master/screens/ReadStoryScreen.js
+++ C71-master (1)/C71-master/screens/ReadStoryScreen.js
@@ -1,23 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View,Image } from 'react-native';
-import {createAppContainer} from 'react-navigation'
-import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {Text,View,TouchableOpacity,StyleSheet,ScrollView,FlatList} from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import db from '../config'
 
-export default class ReadStoryScreen extends React.Component{
-    render(){
-      return (
-        <View style={styles.container}>
-          <Text>Read Story</Text>
-        </View>
-      );
+
+export default class ReadScreen extends React.Component{
+    constructor(){
+    super();
+    this.state = {
+        search: '',
+        dataSource:'',
+      };
     }
     
-  }
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: 'red',
-     
-    },
-  });
+      
+
+        retrieveStories = async()=>{
+            const query = await db.collection("Stories").get()
+            query.docs.map((doc)=>{
+                this.setState({
+                    dataSource:[doc.data()]
+                })
+                console.log(doc.data)
+            })
+        }
+    
+    
+
+
+    render(){
+        if(this.state.search !== ''){
+            this.retrieveStories();
+        }
+        return(
+            
+                      
+            <FlatList            
+            data={this.state.dataSource}
+            renderItem={({item})=>(
+            <View style={{borderBottomWidth:2}}>
+            <SearchBar
+               placeholder="Search Here..."
+               onChangeText={(text)=>{
+                   this.setState({search:text})
+               }}
+               value={this.state.search}
+               style={{marginTop:15}}
+            />  
+                <Text>{"Title:" + item.Title}</Text>
+                <Text>{"Author:" + item.Author}</Text>
+            </View>
+        )}
+        keyExtractor={(item,index)=>index.toString()}
+        onEndReached={this.retrieveStories}
+        onEndReachedThreshold={0.7}      
+            />
+        )
+    }
+}
